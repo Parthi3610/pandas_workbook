e129fb7862986e6f34703ab114a5f6648dd9787f
Guest Login <parthi_3610@yahoo.co.in>
2020-11-04 22:13:32 -0500

    Commit 11

--- outlook_review/outlook_calendar_reader.py
+++ outlook_review/outlook_calendar_reader.py
@@ -9,15 +9,15 @@ import calendar
 def main():
     print("Accessing outlook calendar, please wait....")
     outlook = win32com.client.Dispatch("Outlook.Application")
-    ns = outlook.Namespace("MAPI")
-    appointments = ns.GetDefultFolder(9).Items
-    appointments.Sort["Start"]
+    ns = outlook.GetNamespace("MAPI")
+    appointments = ns.GetDefaultFolder(9).Items
+    appointments.Sort("[Start]")
     appointments.IncludeRecurrences = "True"
 
     begin = InputDate("start")
     end = InputDate("end")
 
-    appointments = appointments.Restrict["[Start] >='"+begin+"' AND [END] <= '"+end+"'"]
+    appointments = appointments.Restrict("[Start] >= '"+begin+"' AND [END] <= '"+end+"'")
     appointmentDictionary = {}
 
     timeregex = re.compile('\d\d/\d\d/\d\d')
@@ -46,5 +46,66 @@ def main():
             appointmentDictionary[subject]={"Subject":subject,"Body": body, "Meetings":[date.strftime("%m%d%y")],
                                             "Times": [time.strftime("%I:%M %p")], "Durations": [duration], "Participants": participants
                                             }
+
+
+    resultsfile = open("resultsTally.csv",'wb')
+    fields = ["Subject", "Body", "Number of Occurences", "Date (First)", "Time (First)", "Duration (First)", "Date (Second)", "Time (Second)", "Duration (Second)", "Date (Third)", "Time (Third)", "Duration (Third)", "Further Dates", "Further Times", "Further Durations", "Participants"]
+    resultsWriter = csv.DictWriter(resultsfile,fields)
+    resultsWriter.writeheader()
+
+    for subject in appointmentDictionary.keys():
+        rowDict = {}
+        rowDict["subject"] = appointmentDictionary[subject]["Subject"] if appointmentDictionary[subject]["Subject"] else ""
+        rowDict["body"] = appointmentDictionary[subject]["Body"] if appointmentDictionary[subject]["Body"] else ""
+        rowDict["participants"] = ", ".join(appointmentDictionary[subject]["Participants"]) if appointmentDictionary[subject]["Participants"] else ""
+        MeetingWriter(rowDict,appointmentDictionary[subject]["Meetings"], appointmentDictionary[subject]["Times"], appointmentDictionary[subject]["Durations"])
+        rowDict["Number of Occurences"] = len(appointmentDictionary[subject]["Meetings"])
+
+        resultsWriter.writerow(rowDict)
+
+def MeetingWriter(rowDict, meetings, times, durations):
+    datecount = 0
+    for i in range(0,len(meetings)):
+        if datecount == 0:
+            rowDict["Date(First)"] = meetings[i]
+            rowDict["Time(First)"] = times[i]
+            rowDict["Duration(First)"] = durations[i]
+
+        elif datecount == 1:
+            rowDict["Date(Second)"] = meetings[i]
+            rowDict["Time(Second)"] = times[i]
+            rowDict["Duration(Second)"] = durations[i]
+
+        elif datecount == 2:
+            rowDict["Date(Third)"] = meetings[i]
+            rowDict["Time(Third)"] = times[i]
+            rowDict["Duration(Third)"] = durations[i]
+
+        else:
+            if "Further Dates" in rowDict.keys():
+                rowDict["Further Dates"] += "," + meetings[i]
+                rowDict["Further Dates"] += "," + times[i]
+                rowDict["Further Dates"] += "," + durations[i]
+            else:
+                rowDict["Further Dates"] = meetings[i]
+                rowDict["Further Dates"] = times[i]
+                rowDict["Further Dates"] = durations[i]
+
+            datecount +=1
+        return rowDict
+
+def InputDate(startOrEnd):
+    ifValid = False
+    while not ifValid:
+        inp = input("Please enter the date " + startOrEnd + " the tally (mm/dd/yyyy):")
+        try:
+            parsedInput = parse(inp).date()
+            ifValid = True
+        except:
+            print("The date you entered could not be processed")
+        return parsedInput.strftime("%m/%d/%Y")
+
+if __name__ == "__main__":
+    main()
 
 
